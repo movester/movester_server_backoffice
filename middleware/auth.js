ebd389fe5ca56d0166f12e6d5d402734b6928e54
jnhro1 <jnhro1@gmail.com>
2021-08-23 16:19:06 +0900

    implement notice-create

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,6 @@ const utils = require("../utils/utils");
 const verifyToken = (req, res, next) => {
     try {
         const accessToken = req.headers.authorization.split("Bearer ")[1];
-
         const decodeAccessToken = jwt.verify(
             accessToken,
             process.env.JWT_ACCESS_SECRET
@@ -44,10 +43,11 @@ const verifyRefreshToken = (req, res, next) => {
         redisClient.get(decodeRefreshToken.sub.toString(), (err, data) => {
             if (err) throw err;
 
-            if (data === null)
+            if (data === null) {
                 return res
                     .status(statusCode.BAD_REQUEST)
                     .json(utils.successFalse(responseMessage.TOKEN_EMPTY));
+            }
             if (JSON.parse(data).token != refreshToken) {
                 return res
                     .status(statusCode.BAD_REQUEST)
