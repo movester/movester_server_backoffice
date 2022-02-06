6a057c939fd3b7159a3ae9a1bde0a3b6012d36a1
jnhro1 <jnhro1@gmail.com>
2022-02-07 03:37:43 +0900

    #20 Fix: admin logout REST POST to GET

--- routes/admin.js
+++ routes/admin.js
@@ -11,6 +11,6 @@ router.get('/', auth.checkToken, (req, res) => res.send('인증 완료'));
 router.post('/join', validator.join, validatorError.error, userCtrl.join);
 router.post('/login', validator.login, validatorError.error, userCtrl.login);
 router.patch('/password/:adminIdx', auth.checkToken, validator.updatePassword, validatorError.error, userCtrl.updatePassword);
-router.post('/logout', auth.checkToken, userCtrl.logout);
+router.get('/logout', auth.checkToken, userCtrl.logout);
 
 module.exports = router;
