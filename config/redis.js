c85e0945f2defee30b2fef8067fd247c80499e3e
jnhro1 <jnhro1@gmail.com>
2022-02-06 21:47:26 +0900

    #18 CR: 코드리뷰 반영

--- config/redis.js
+++ config/redis.js
@@ -1,6 +1,6 @@
 const redis = require('redis');
 
-const redisClient = redis.createClient({ host: '127.0.0.1', port: process.env.REDIS_PORT, db: 0 });
+const redisClient = redis.createClient({ host: process.env.host, port: process.env.REDIS_PORT, db: 0 });
 
 redisClient.on('connect', () => {
   console.log('redis client connected');
--- index.js
+++ index.js
@@ -20,27 +20,4 @@ app.use(cookieParser());
 app.use('/api', router);
 app.use('/uploads', express.static('uploads'));
 
-let users = [
-  { id: 1, name: 'a' },
-  { id: 2, name: 'b' },
-  { id: 3, name: 'c' },
-];
-
-app.get('/users/:id', (req, res) => {
-  const id = +req.params.id;
-  if (Number.isNaN(id)) return res.status(400).end();
-  const user = users.find(user => user.id === id);
-  if (!user) return res.status(404).end();
-  res.json(user);
-});
-
-app.post('/users', (req, res) => {
-  const { name } = req.body;
-  if (!name) return res.status(400).end();
-  if (users.find(user => user.name === name)) return res.status(409).end();
-  const id = users.length + 1;
-  const user = { id, name };
-  users = [...users, user];
-  res.status(201).json(user);
-});
 module.exports = app;
