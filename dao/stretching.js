8e708526fc36ee91eaa887414cec7e231d8c9776
jnhro1 <jnhro1@gmail.com>
2022-03-28 16:30:57 +0900

    #82 Fix: 스트레칭 dao 변경

--- dao/stretching.js
+++ dao/stretching.js
@@ -19,7 +19,7 @@ const createStretching = async ({
     await conn.beginTransaction();
 
     const insertStretching = `INSERT
-                                INTO stretching (title, contents, main_body, sub_body, tool, youtube_url, image, writer)
+                                INTO stretching (title, contents, main_body, sub_body, tool, youtube_url, image, admin_idx)
                               VALUES ('${title}', '${contents}', ${mainBody}, ${subBody}, ${tool}, '${youtubeUrl}', '${image}', ${adminIdx});`;
     const [insertRow] = await conn.query(insertStretching);
 
@@ -52,7 +52,10 @@ const findStretchingByTitle = async title => {
   let conn;
   try {
     conn = await pool.getConnection(async conn => conn);
-    const sql = `SELECT stretching_idx AS 'stretchingIdx', title, main_body AS mainBody, sub_body AS subBody
+    const sql = `SELECT stretching_idx AS 'stretchingIdx'
+                      , title
+                      , main_body AS mainBody
+                      , sub_body AS subBody
                    FROM stretching
                   WHERE title = '${title}'`;
     const [row] = await conn.query(sql);
@@ -69,7 +72,10 @@ const findStretchingByIdx = async stretchingIdx => {
   let conn;
   try {
     conn = await pool.getConnection(async conn => conn);
-    const sql = `SELECT stretching_idx AS 'stretchingIdx', title, main_body AS 'mainBody', sub_body AS 'subBody'
+    const sql = `SELECT stretching_idx AS 'stretchingIdx'
+                      , title
+                      , main_body AS 'mainBody'
+                      , sub_body AS 'subBody'
                    FROM stretching
                   WHERE stretching_idx = ${stretchingIdx}`;
     const [row] = await conn.query(sql);
@@ -109,19 +115,30 @@ const getDetailStretching = async stretchingIdx => {
     conn = await pool.getConnection(async conn => conn);
     await conn.beginTransaction();
 
-    const stretchingSql = `SELECT stretching_idx AS 'stretchingIdx', title, contents, main_body AS mainBody, sub_body AS subBody, tool, youtube_url, image, writer, DATE_FORMAT(create_at,'%Y.%m.%d') AS 'createAt'
+    const stretchingSql = `SELECT stretching_idx AS 'stretchingIdx'
+                                , title
+                                , contents
+                                , main_body AS mainBody
+                                , sub_body AS subBody
+                                , tool
+                                , youtube_url
+                                , image
+                                , admin_idx AS adminIdx
+                                , DATE_FORMAT(create_at,'%Y.%m.%d') AS 'createAt'
                              FROM stretching
                             WHERE stretching_idx = ${stretchingIdx}`;
     const [stretching] = await conn.query(stretchingSql);
 
     if (!stretching.length) return null;
 
-    const effectSql = ` SELECT stretching_idx AS 'stretchingIdx', effect_type AS effect
+    const effectSql = ` SELECT stretching_idx AS 'stretchingIdx'
+                             , effect_type AS effect
                           FROM stretching_effect
                          WHERE stretching_idx = ${stretchingIdx}`;
     const [effect] = await conn.query(effectSql);
 
-    const postureSql = ` SELECT stretching_idx AS 'stretchingIdx', posture_type AS posture
+    const postureSql = ` SELECT stretching_idx AS 'stretchingIdx'
+                              , posture_type AS posture
                            FROM stretching_posture
                           WHERE stretching_idx = ${stretchingIdx}`;
     const [posture] = await conn.query(postureSql);
@@ -148,7 +165,14 @@ const updateStretching = async stretching => {
     await conn.beginTransaction();
 
     const updateStretching = `UPDATE stretching
-                                 SET title = '${stretching.title}', contents = '${stretching.contents}', main_body = ${stretching.mainBody}, sub_body = ${stretching.subBody}, tool = ${stretching.tool}, youtube_url = '${stretching.youtubeUrl}', image = '${stretching.image}', writer = ${stretching.adminIdx}
+                                 SET title = '${stretching.title}'
+                                   , contents = '${stretching.contents}'
+                                   , main_body = ${stretching.mainBody}
+                                   , sub_body = ${stretching.subBody}
+                                   , tool = ${stretching.tool}
+                                   , youtube_url = '${stretching.youtubeUrl}'
+                                   , image = '${stretching.image}'
+                                   , admin_idx = ${stretching.adminIdx}
                                WHERE stretching_idx = ${stretching.stretchingIdx}`;
     const [updateRow] = await conn.query(updateStretching);
 
