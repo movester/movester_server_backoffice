2540fb73933ffbed93c08f6f6fd730ef10077c3a
jnhro1 <jnhro1@gmail.com>
2022-05-10 20:45:51 +0900

    #92 Fix: title 오타

--- middleware/validator/week.js
+++ middleware/validator/week.js
@@ -5,7 +5,7 @@ const createWeek = [
     .exists()
     .withMessage('title을(를) 입력해주세요.')
     .isLength({ min: 2, max: 20 })
-    .withMessage('title은 최소 5글자부터 최대 20글자까지 가능합니다.'),
+    .withMessage('title은 최소 2글자부터 최대 20글자까지 가능합니다.'),
   body('week', 'week 는 숫자로만 이루어진 길이가 7인 배열이어야 합니다.')
     .isArray({ min: 7, max: 7 })
     .custom(v => v.every(e => e > 0)),
