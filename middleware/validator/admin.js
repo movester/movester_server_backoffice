abb2a1078065ac173efa0c056b5c6e79e2cefd27
jnhro1 <jnhro1@gmail.com>
2022-05-10 17:54:25 +0900

    #92 Fix: 관리자 계정 등록 validator

--- middleware/validator/admin.js
+++ middleware/validator/admin.js
@@ -4,18 +4,18 @@ const join = [
   body('id')
     .exists()
     .withMessage('아이디를 입력해주세요.')
-    .isLength({ min: 5, max: 10 })
-    .withMessage('아이디는 최소 5글자부터 최대 10글자까지 가능합니다.'),
+    .matches(/^[a-zA-Z0-9]{5,10}$/)
+    .withMessage('아이디는 영문, 숫자로 조합된 5자리 이상 10자리 이하로 입력해주세요.'),
   body('password')
-    .exists()
+    .notEmpty()
     .withMessage('비밀번호를 입력해주세요.')
-    .isLength({ min: 5, max: 20 })
-    .withMessage('비밀번호는 최소 5글자부터 최대 20글자까지 가능합니다.'),
+    .matches(/^(?=.*[a-zA-Z])(?=.*[0-9]).{8,20}$/)
+    .withMessage('비밀번호는 영문, 숫자를 반드시 포함하여 8자리 이상 20자리 이하로 입력해주세요.'),
   body('name')
     .exists()
     .withMessage('이름 입력해주세요.')
-    .isLength({ min: 2, max: 12 })
-    .withMessage('이름은 최소 2글자부터 최대 12글자까지 가능합니다.'),
+    .matches(/^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]{2,8}$/)
+    .withMessage('이름은 한글, 영문, 숫자로 조합된 2자리 이상 8자리 이하로 입력해주세요.'),
   body('rank')
     .exists()
     .withMessage('등급을 입력해주세요.')
@@ -28,31 +28,31 @@ const login = [
   body('id')
     .exists()
     .withMessage('아이디를 입력해주세요.')
-    .isLength({ min: 5, max: 10 })
-    .withMessage('아이디는 최소 5글자부터 최대 10글자까지 가능합니다.'),
+    .matches(/^[a-zA-Z0-9]{5,10}$/)
+    .withMessage('아이디는 영문, 숫자로 조합된 5자리 이상 10자리 이하로 입력해주세요.'),
   body('password')
-    .exists()
+    .notEmpty()
     .withMessage('비밀번호를 입력해주세요.')
-    .isLength({ min: 5, max: 20 })
-    .withMessage('비밀번호는 최소 5글자부터 최대 20글자까지 가능합니다.'),
+    .matches(/^(?=.*[a-zA-Z])(?=.*[0-9]).{8,20}$/)
+    .withMessage('비밀번호는 영문, 숫자를 반드시 포함하여 8자리 이상 20자리 이하로 입력해주세요.'),
 ];
 
 const updatePassword = [
   body('beforePassword')
     .exists()
     .withMessage('기존 비밀번호를 입력해주세요.')
-    .isLength({ min: 5, max: 20 })
-    .withMessage('비밀번호는 최소 5글자부터 최대 20글자까지 가능합니다.'),
+    .matches(/^(?=.*[a-zA-Z])(?=.*[0-9]).{8,20}$/)
+    .withMessage('비밀번호는 영문, 숫자를 반드시 포함하여 8자리 이상 20자리 이하로 입력해주세요.'),
   body('newPassword')
     .exists()
     .withMessage('새로운 비밀번호를 입력해주세요.')
-    .isLength({ min: 5, max: 20 })
-    .withMessage('비밀번호는 최소 5글자부터 최대 20글자까지 가능합니다.'),
+    .matches(/^(?=.*[a-zA-Z])(?=.*[0-9]).{8,20}$/)
+    .withMessage('비밀번호는 영문, 숫자를 반드시 포함하여 8자리 이상 20자리 이하로 입력해주세요.'),
   body('confirmPassword')
     .exists()
     .withMessage('비밀번호 확인값을 입력해주세요.')
-    .isLength({ min: 5, max: 20 })
-    .withMessage('비밀번호는 최소 5글자부터 최대 20글자까지 가능합니다.'),
+    .matches(/^(?=.*[a-zA-Z])(?=.*[0-9]).{8,20}$/)
+    .withMessage('비밀번호는 영문, 숫자를 반드시 포함하여 8자리 이상 20자리 이하로 입력해주세요.'),
 ];
 
 module.exports = {
